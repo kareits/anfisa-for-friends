484b7750890d218f6b66a05f3536459879385769
kareits <mail.igor.li@gmail.com>
2023-05-26 21:36:10 +0600

    Change 15

--- anfisa_for_friends/settings.py
+++ anfisa_for_friends/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,8 +88,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'BASE_DIR/static_dev/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_URL = '/static_dev/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static_dev',
