b4ebd2855535602b218cb1847037cadfbf44f6b2
kareits <mail.igor.li@gmail.com>
2023-05-27 23:24:29 +0600

    Final changes before deployment

--- anfisa_for_friends/settings.py
+++ anfisa_for_friends/settings.py
@@ -30,6 +30,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'anfisa_for_friends.urls'
